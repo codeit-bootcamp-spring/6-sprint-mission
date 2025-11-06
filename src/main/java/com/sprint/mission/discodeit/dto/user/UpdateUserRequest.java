5e72abdd461659f353251ca849604b4fd4b45e4f
HOGUN00 <7080rjs@gmail.com>
2025-11-07 00:24:52 +0900

    Feat: userServiceTest

--- src/main/java/com/sprint/mission/discodeit/dto/user/UpdateUserRequest.java
+++ src/main/java/com/sprint/mission/discodeit/dto/user/UpdateUserRequest.java
@@ -1,7 +1,9 @@
 package com.sprint.mission.discodeit.dto.user;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.Builder;
 
+@Builder
 public record UpdateUserRequest(
     @NotBlank
     String newUsername,
