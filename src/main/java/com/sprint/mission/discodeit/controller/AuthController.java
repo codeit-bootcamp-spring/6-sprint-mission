b809727541e4c659e875c1bff4e8a779cce5832f
jihoon-68 <pkrwlgns64@gmail.com>
2025-12-19 09:32:06 +0900

    미션 9 필요없는 메소드나 파일 삭제

--- src/main/java/com/sprint/mission/discodeit/controller/AuthController.java
+++ src/main/java/com/sprint/mission/discodeit/controller/AuthController.java
@@ -1,20 +1,13 @@
 package com.sprint.mission.discodeit.controller;
 
 import com.sprint.mission.discodeit.controller.api.AuthApi;
-import com.sprint.mission.discodeit.dto.Auth.LoginRequest;
-import com.sprint.mission.discodeit.dto.User.UserDto;
-import com.sprint.mission.discodeit.service.AuthService;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.web.bind.annotation.*;
-
-import java.nio.charset.StandardCharsets;
-import java.util.Locale;
 
 
 @Slf4j
@@ -23,11 +16,6 @@ import java.util.Locale;
 @RestController
 @RequiredArgsConstructor
 public class AuthController implements AuthApi {
-    private final AuthService authService;
-    @PostMapping("login")
-    public ResponseEntity<UserDto> login(@RequestBody LoginRequest loginRequest) {
-        return ResponseEntity.status(HttpStatus.OK).body(authService.login(loginRequest));
-    }
 
     @GetMapping("csrf-token")
     public ResponseEntity<Void> getCsrfToken(CsrfToken csrfToken){
--- src/main/java/com/sprint/mission/discodeit/dto/Auth/LoginRequest.java
+++
@@ -1,7 +0,0 @@
-package com.sprint.mission.discodeit.dto.Auth;
-
-public record LoginRequest(
-        String username,
-        String password
-) {
-}
