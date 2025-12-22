3f1027d520cec62a6ddee36b6d7bd23d6d6b668b
김재원 <dumdum7@hs.ac.kr>
2025-12-22 13:47:08 +0900

    - feat: remember me를 위한 영구 토큰 테이블 추가

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -67,6 +67,13 @@ CREATE TABLE message_attachments
     PRIMARY KEY (message_id, attachment_id)
 );
 
+create table persistent_logins (
+    username varchar(64) not null,
+    series varchar(64) primary key,
+    token varchar(64) not null,
+    last_used timestamp not null
+)
+
 /*
 GRANT
 ALL
