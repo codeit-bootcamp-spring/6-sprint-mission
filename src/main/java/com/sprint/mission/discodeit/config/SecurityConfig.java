d55eb9413ecdfb745ca199608638b6d65824bbea
김태현 <eedcs0926@gmail.com>
2025-12-24 22:35:52 +0900

    feat : csrf보호설정

--- src/main/java/com/sprint/mission/discodeit/config/SecurityConfig.java
+++ src/main/java/com/sprint/mission/discodeit/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @Configuration
 @EnableWebSecurity
@@ -12,6 +13,18 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http)throws Exception{
+        http
+                .csrf(csrf -> csrf
+                        // 쿠키 저장소 설정: 클라이언트 JS가 읽을 수 있도록 HttpOnly를 false로 설정
+                        .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                        // 위에서 만든 SPA 전용 핸들러 등록
+                        .csrfTokenRequestHandler(new SpaCsrfTokenRequestHandler())
+                )
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/auth/csrf-token").permitAll() // 토큰 발급 API 개방
+                        .anyRequest().authenticated()
+                );
+
         return http.build();
     }
 }
