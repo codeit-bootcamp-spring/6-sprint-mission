87bc8539421f323e99ddbe37544c1e26b1f707dd
김태현 <eedcs0926@gmail.com>
2025-12-25 12:32:36 +0900

    feat : 인증(로그아웃)

--- src/main/java/com/sprint/mission/discodeit/config/SecurityConfig.java
+++ src/main/java/com/sprint/mission/discodeit/config/SecurityConfig.java
@@ -5,11 +5,13 @@ import com.sprint.mission.discodeit.security.LoginSuccessHandler;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.logout.HttpStatusReturningLogoutSuccessHandler;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @Configuration
@@ -21,7 +23,7 @@ public class SecurityConfig {
     private final LoginFailureHandler loginFailureHandler;
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http)throws Exception{
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf
                         // 쿠키 저장소 설정 HttpOnly를 false로 설정
@@ -37,6 +39,13 @@ public class SecurityConfig {
                         .loginProcessingUrl("/api/auth/login") // 로그인 처리 URL 설정
                         .successHandler(loginSuccessHandler)   // 성공 핸들러 대체
                         .failureHandler(loginFailureHandler)   // 실패 핸들러 대체
+                )
+                .logout(logout -> logout
+                        .logoutUrl("/api/auth/logout") // 로그아웃 처리 URL 설정
+                        // 성공 시 리다이렉트하지 않고 204 No Content 응답 반환
+                        .logoutSuccessHandler(new HttpStatusReturningLogoutSuccessHandler(HttpStatus.NO_CONTENT))
+                        .deleteCookies("JSESSIONID") // 로그아웃 시 세션 쿠키 삭제
+                        .invalidateHttpSession(true)
                 );
 
         return http.build();
