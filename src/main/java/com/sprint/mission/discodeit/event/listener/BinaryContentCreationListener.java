16d6f8510041a69e4b36d69d97039fe8de2e2918
김재원 <dumdum7@hs.ac.kr>
2026-01-06 17:26:56 +0900

    - feat: 메타 데이터에 성공 여부 반영하는 로직 추가

--- src/main/java/com/sprint/mission/discodeit/event/listener/BinaryContentCreationListener.java
+++ src/main/java/com/sprint/mission/discodeit/event/listener/BinaryContentCreationListener.java
@@ -1,6 +1,8 @@
 package com.sprint.mission.discodeit.event.listener;
 
+import com.sprint.mission.discodeit.entity.enums.BinaryContentStatus;
 import com.sprint.mission.discodeit.event.event.BinaryContentCreatedEvent;
+import com.sprint.mission.discodeit.service.BinaryContentService;
 import com.sprint.mission.discodeit.storage.BinaryContentStorage;
 import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.Async;
@@ -13,12 +15,20 @@ import org.springframework.transaction.event.TransactionalEventListener;
 public class BinaryContentCreationListener {
 
   private final BinaryContentStorage binaryContentStorage;
+  private final BinaryContentService binaryContentService;
 
   @Async
   @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
   public void handleBinaryContentCreatedEvent(BinaryContentCreatedEvent event) {
 
-    binaryContentStorage.put(event.getId(), event.getBinaryContent());
+    try {
+      binaryContentStorage.put(event.getId(), event.getBinaryContent());
+
+      binaryContentService.updateBinaryContentStatus(event.getId(), BinaryContentStatus.SUCCESS);
+
+    } catch (Exception e) {
+      binaryContentService.updateBinaryContentStatus(event.getId(), BinaryContentStatus.FAIL);
+    }
 
   }
 
