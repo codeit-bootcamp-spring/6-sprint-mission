11456c5807e6cfa13f21168bd4e72d3b79571b7b
김재원 <dumdum7@hs.ac.kr>
2026-01-06 15:30:29 +0900

    - fix: 트랜잭션 기반 이벤트로 변경

--- src/main/java/com/sprint/mission/discodeit/event/listener/BinaryContentCreationListener.java
+++ src/main/java/com/sprint/mission/discodeit/event/listener/BinaryContentCreationListener.java
@@ -3,9 +3,10 @@ package com.sprint.mission.discodeit.event.listener;
 import com.sprint.mission.discodeit.event.event.BinaryContentCreatedEvent;
 import com.sprint.mission.discodeit.storage.BinaryContentStorage;
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.event.TransactionPhase;
+import org.springframework.transaction.event.TransactionalEventListener;
 
 @Component
 @RequiredArgsConstructor
@@ -14,7 +15,7 @@ public class BinaryContentCreationListener {
   private final BinaryContentStorage binaryContentStorage;
 
   @Async
-  @EventListener
+  @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
   public void handleBinaryContentCreatedEvent(BinaryContentCreatedEvent event) {
 
     binaryContentStorage.put(event.getId(), event.getBinaryContent());
