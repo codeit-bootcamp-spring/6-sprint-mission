560ad666590fbf0e38ae8f32e1d337448e439f8e
김재원 <dumdum7@hs.ac.kr>
2026-01-07 13:16:20 +0900

    - feat: BasicNotificationService 생성

--- src/main/java/com/sprint/mission/discodeit/repository/NotificationRepository.java
+++ src/main/java/com/sprint/mission/discodeit/repository/NotificationRepository.java
@@ -1,9 +1,12 @@
 package com.sprint.mission.discodeit.repository;
 
 import com.sprint.mission.discodeit.entity.NotificationEntity;
+import java.util.List;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface NotificationRepository extends JpaRepository<NotificationEntity, UUID> {
+
+  List<NotificationEntity> findByReceiverId(UUID receiverId);
 
 }
---
+++ src/main/java/com/sprint/mission/discodeit/service/basic/BasicNotificationService.java
@@ -0,0 +1,40 @@
+package com.sprint.mission.discodeit.service.basic;
+
+import com.sprint.mission.discodeit.dto.NotificationDTO;
+import com.sprint.mission.discodeit.entity.NotificationEntity;
+import com.sprint.mission.discodeit.mapper.NotificationMapper;
+import com.sprint.mission.discodeit.repository.NotificationRepository;
+import com.sprint.mission.discodeit.service.NotificationService;
+import java.util.List;
+import java.util.UUID;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class BasicNotificationService implements NotificationService {
+
+  private final NotificationRepository notificationRepository;
+  private final NotificationMapper notificationMapper;
+
+  @Override
+  public List<NotificationDTO> findAllNotificationsByUserId(UUID userId) {
+
+    List<NotificationEntity> notificationEntityList = notificationRepository
+        .findByReceiverId(userId);
+
+    return notificationEntityList.stream().map(
+        notificationMapper::toDTO
+    ).toList();
+
+  }
+
+  @Override
+  public void deleteNotificationById(UUID id) {
+
+    notificationRepository.deleteById(id);
+
+  }
+}
