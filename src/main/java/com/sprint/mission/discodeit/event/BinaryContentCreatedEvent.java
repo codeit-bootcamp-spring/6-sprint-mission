0d06949821e970af0ff8cbe97359fd6cd1970011
HOGUN00 <7080rjs@gmail.com>
2026-01-13 01:35:24 +0900

    refactor: BinaryContentCreatedEvent

--- src/main/java/com/sprint/mission/discodeit/event/BinaryContentCreatedEvent.java
+++ src/main/java/com/sprint/mission/discodeit/event/BinaryContentCreatedEvent.java
@@ -1,11 +1,28 @@
 package com.sprint.mission.discodeit.event;
 
+import java.util.Arrays;
+import java.util.Objects;
 import java.util.UUID;
 
-// 배열 필드있으면 equals, hashcode 문제 발생할 수 있음?
 public record BinaryContentCreatedEvent(
     UUID binaryContentId,
     byte[] file
 ) {
 
+  @Override
+  public boolean equals(Object o) {
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    BinaryContentCreatedEvent that = (BinaryContentCreatedEvent) o;
+    return Arrays.equals(file, that.file) && Objects.equals(binaryContentId,
+        that.binaryContentId);
+  }
+
+  @Override
+  public int hashCode() {
+    int result = Objects.hash(binaryContentId);
+    result = 31 * result + Arrays.hashCode(file);
+    return result;
+  }
 }
